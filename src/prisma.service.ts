0da9f3b21342e664b78559ab499955cd818dc172
hayeon7898 <hayeon22@korea.ac.kr>
2025-08-27 22:22:17 +0900

    add: db connection log(prisma.service.ts)

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -5,6 +5,7 @@ import { PrismaClient } from '@prisma/client';
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   async onModuleInit() {
     await this.$connect();
+    console.log('✅ Database connected!'); // 연결 성공 확인 로그
   }
 
   async onModuleDestroy() {
