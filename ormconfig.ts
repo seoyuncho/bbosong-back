8cf5d8fedb71236b60c4b050eeb64c1d1f2f916f
hayeon7898 <hayeon22@korea.ac.kr>
2025-08-29 11:20:53 +0900

    delete: ormconfig page

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,14 +1,14 @@
-import { DataSource } from 'typeorm'
+// import { DataSource } from 'typeorm'
 
-export const AppDataSource = new DataSource({
-  type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: 'seoyunch0^^',
-  database: 'bbosong',
-  entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  synchronize: false,
-  migrations: [__dirname + '/**/migrations/*.js'],
-  migrationsTableName: 'migrations',
-});
+// export const AppDataSource = new DataSource({
+//   type: 'mysql',
+//   host: 'localhost',
+//   port: 3306,
+//   username: 'root',
+//   password: 'seoyunch0^^',
+//   database: 'bbosong',
+//   entities: [__dirname + '/**/*.entity{.ts,.js}'],
+//   synchronize: false,
+//   migrations: [__dirname + '/**/migrations/*.js'],
+//   migrationsTableName: 'migrations',
+// });
