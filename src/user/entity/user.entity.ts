d76cf7b4f32e7739fd839ce7210afea6d3222a83
hayeon7898 <hayeon22@korea.ac.kr>
2025-08-29 12:12:08 +0900

    delete: createUserTable, user.entity pages(주석처리)

--- src/user/entity/user.entity.ts
+++ src/user/entity/user.entity.ts
@@ -1,19 +1,19 @@
-import { Column, Entity, PrimaryColumn } from 'typeorm';
+// import { Column, Entity, PrimaryColumn } from 'typeorm';
 
-@Entity('User')
-export class UserEntity {
-  @PrimaryColumn()
-  id: string;
+// @Entity('User')
+// export class UserEntity {
+//   @PrimaryColumn()
+//   id: string;
 
-  @Column({ length: 30 })
-  name: string;
+//   @Column({ length: 30 })
+//   name: string;
 
-  @Column({ length: 60 })
-  email: string;
+//   @Column({ length: 60 })
+//   email: string;
 
-  @Column({ length: 30 })
-  password: string;
+//   @Column({ length: 30 })
+//   password: string;
 
-  @Column({ length: 60 })
-  signupVerifyToken: string;
-}
+//   @Column({ length: 60 })
+//   signupVerifyToken: string;
+// }
